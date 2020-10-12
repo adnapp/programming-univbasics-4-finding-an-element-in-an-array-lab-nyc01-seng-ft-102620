9f436ed53dd5de21bd6a13f57751e87e4d7490d0
Adam Napoletano <adam.napps@gmail.com>
2020-10-12 18:48:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,10 +3,9 @@ def find_element_index(array, value_to_find)
 count = 0
 while count < array.length
  if array[count] == value_to_find
-   break
+   return count
  else
    count += 1
 end
-return count
 end
 end
